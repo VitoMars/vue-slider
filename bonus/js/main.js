37767dcbff7f053e07196d83be77e78542460f88
Vito <vito95v@gmail.com>
2021-09-08 17:15:43 +0200

    Inserimento primo bonus

---
+++ bonus/js/main.js
@@ -0,0 +1,28 @@
+Vue.config.devtools = true;
+
+const app = new Vue({
+   el: "#root",
+   data: {
+      counterPhoto: 0,
+      photos: [
+         "img/image1.jpg",
+         "img/image2.jpg",
+         "img/image3.jpg",
+         "img/image4.jpg",
+      ],
+   },
+   methods: {
+      prevPhoto() {
+         this.counterPhoto -= 1;
+         if (this.counterPhoto < 0) {
+            this.counterPhoto = this.photos.length - 1;
+         }
+      },
+      nextPhoto() {
+         this.counterPhoto += 1;
+         if (this.counterPhoto > this.photos.length - 1) {
+            this.counterPhoto = 0;
+         }
+      },
+   },
+});
